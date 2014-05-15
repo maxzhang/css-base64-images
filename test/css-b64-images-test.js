8f28e0c1f6ef20ad25392d25e957ff3d13ed33a9
maxzhang <zhangdaiping@gmail.com>
2014-05-15 10:39:27 +0800

    增加对html <img>标签base64编码的处理

--- test/css-b64-images-test.js
+++ test/css-b64-images-test.js
@@ -1,38 +1,54 @@
 var Path = require('path'),
-  fs = require('fs'),
-  b64 = require('..');
+    fs = require('fs'),
+    b64 = require('..');
 
 require('should');
 
-describe('A complex CSS', function(){
-  var cssFile = Path.join(__dirname, 'fixture', 'css', 'style.css'),
-    relative = Path.join(__dirname, 'fixture', 'css');
-    root = Path.join(__dirname, 'fixture');
+describe('A complex CSS', function() {
+    var cssFile = Path.join(__dirname, 'fixture', 'css', 'style.css'),
+        htmlFile = Path.join(__dirname, 'fixture', 'html', 'test.html'),
+        relative = Path.join(__dirname, 'fixture', 'css'),
+        root = Path.join(__dirname, 'fixture');
 
-  it('a file should be optimized with base64', function(done){
-    b64.fromFile(cssFile, root, function(err, css){
-      cssShouldBeCorrect(css);
-      done();
+    it('a file should be optimized with base64', function(done) {
+        b64.fromFile(cssFile, root, function(err, css) {
+            cssShouldBeCorrect(css);
+            done();
+        });
     });
-  });
 
-  it('a string should be optimized with base64', function(done){
-    var css = fs.readFileSync(cssFile);
-    b64.fromString(css, relative, root, function(err, css){
-      cssShouldBeCorrect(css);
-      done();
+    it('a string should be optimized with base64', function(done) {
+        var css = fs.readFileSync(cssFile);
+        b64.fromString(css, relative, root, function(err, css) {
+            cssShouldBeCorrect(css);
+            done();
+        });
     });
-  });
+
+    it('a html file should be optimized with base64', function(done) {
+        b64.fromFile(htmlFile, root, function(err, html) {
+            htmlShouldBeCorrect(html);
+            done();
+        });
+    });
 });
 
-function cssShouldBeCorrect(css){
-  css.should.include(".single-quote {\n  background: url('data:image/gif;base64,");
-  css.should.include(".double-quote {\n  background: url(\"data:image/gif;base64,");
-  css.should.include(".absolute {\n  background: url('data:image/gif;base64,");
+function cssShouldBeCorrect(css) {
+    css.should.include(".single-quote {\n  background: url('data:image/gif;base64,");
+    css.should.include(".double-quote {\n  background: url(\"data:image/gif;base64,");
+    css.should.include(".absolute {\n  background: url('data:image/gif;base64,");
 
-  css.should.include(".external {\n  background: url('http");
-  css.should.include(".tooBig {\n  background: url('../img");
-  css.should.include(".not-found {\n  background: url('../img");
+    css.should.include(".external {\n  background: url('http");
+    css.should.include(".tooBig {\n  background: url('../img");
+    css.should.include(".not-found {\n  background: url('../img");
 
-  css.should.include(".mediatype {\n  background: url('data:image/svg+xml;base64,");
+    css.should.include(".mediatype {\n  background: url('data:image/svg+xml;base64,");
+}
+
+function htmlShouldBeCorrect(html) {
+    html.should.include('<img alt="gif" src="data:image/gif;base64,');
+    html.should.include('<img alt="not-found" src="../img');
+    html.should.include('<img alt="svg" src="data:image/svg+xml;base64,');
+
+    html.should.include('<script type="text/javascript" src="main.js"></script>');
 }
